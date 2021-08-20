dd4eca67ac54547f9cd0e72f5090a23b17fb5e56
cosolgit <1075884015@qq.com>
2021-08-20 21:07:45 +0800

    first commit

---
+++ src/core/instance/index.js
@@ -0,0 +1,20 @@
+import { initMixin } from './init'
+import { stateMixin } from './state'
+import { renderMixin } from './render'
+import { eventsMixin } from './events'
+import { lifecycleMixin } from './lifecycle'
+import { warn } from '../util/index'
+
+// Vue 构造函数
+function Vue (options) {
+  //Vue.prototype._init 方法
+  this._init(options)
+}
+
+initMixin(Vue)
+stateMixin(Vue)
+eventsMixin(Vue)
+lifecycleMixin(Vue)
+renderMixin(Vue)
+
+export default Vue
